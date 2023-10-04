c1c8929ead57463fcbcab70012fbbd9896fc1907
MoonlightDreamer <sdmagomedov@edu.hse.ru>
2023-10-05 01:19:42 +0300

    spring data jpa db work

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,7 +1,15 @@
 package tacos.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, String> {
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 }
